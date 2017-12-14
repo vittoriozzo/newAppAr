edddda21a77c73945be0bcaa95d7b5850b9f556d
Vittorio Grimaldi <vitt.grim@gmail.com>
2017-12-14 17:28:25 +0100

    Initial Commit

---
+++ newAppAr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newAppAr
+//
+//  Created by Vittorio Grimaldi on 14/12/17.
+//  Copyright © 2017 Vittorio Grimaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
